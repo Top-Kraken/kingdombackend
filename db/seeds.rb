7790ca41d35e7d6df9e116ad8ada91264ec84249
KingdomParadise <ericlee.apollo@gmail.com>
2021-07-20 09:16:58 +0000

    linux changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,17 +19,21 @@
 
 ##
 # ##create subscription
-Subscription.create!(name: 'Main', price: '195.99', detail: '195.99 for the first month, the 175.99 each month')
+Subscription.find_or_create_by(price: 194.99, second_price: 174.99, name: "second_plan", detail: "$194.99 for the first month and then $174.99 every month after")
+Subscription.find_or_create_by(price: 244.99, second_price: 224.99, name: "second_plan", detail: "$244.99 for the first month and then $ 224.99 every month after")
+Subscription.find_or_create_by(price: 564.99, second_price: 544.99, name: "second_plan", detail: "$564.99 for the first month and then $ 5444.99 every month after")
 
-user = User.create phone_number: '+12025550185', password: '123456', password_confirmation: '123456', full_name: 'Jack Doe', status: 'active'
-
-UserSubscription.find_or_create_by(subscription_id: '1', user_id: user.id, stripe_subscription: '123', status: true)
-
-lead = user.leads.create first_name: 'John', phone_number: '+12025550186',
-                         heat: 0, stage: 0,
-                         facebook: 'https://www.facebook.com'
-
-[*DateTime.now..(DateTime.now + 2.months)].each do |datetime|
-  start_datetime = [datetime + 10.minutes, datetime + 20.minutes, datetime + 30.minutes]
-  Demo.create(start_datetime: start_datetime.sample, lead: lead, user: user)
+user = User.find_by(phone_number: '+12025550185')
+unless user
+  user = User.create phone_number: '+12025550185', password: '123456', password_confirmation: '123456', full_name: 'Jack Doe', status: 'active'
+  UserSubscription.find_or_create_by(subscription_id: '1', user_id: user.id, stripe_subscription: '123', status: true)
+  
+  lead = user.leads.create first_name: 'John', phone_number: '+12025550186',
+                           heat: 0, stage: 0,
+                           facebook: 'https://www.facebook.com'
+  
+  [*DateTime.now..(DateTime.now + 2.months)].each do |datetime|
+    start_datetime = [datetime + 10.minutes, datetime + 20.minutes, datetime + 30.minutes]
+    Demo.create(start_datetime: start_datetime.sample, lead: lead, user: user)
+  end
 end
