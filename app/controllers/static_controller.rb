4b5cd39fc61cd9efd37d23cef74c6d74e14a03dd
timkern0521 <timkern.lancer@gmail.com>
2021-07-21 19:16:00 -0700

    sms sending

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -18,7 +18,7 @@ class StaticController < ApplicationController
   end
 
   def add_lead
-    filePath = "#{request.protocol}" + "#{request.host_with_port}" + "/Deus Blueprint.pdf"
+    filePath = "#{request.protocol}" + "#{request.host_with_port}" + "/apple-touch-icon.png"
     @lead = current_user.leads.build(lead_params)
     @lead.added_from = "web_form"
     @lead.send_document_nofication(filePath)
