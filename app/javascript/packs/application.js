b25d43887d2f6a53053a5a4bca264bcf8e315ff5
KingdomParadise <ericlee.apollo@gmail.com>
2021-07-22 13:02:34 -0700

    show password fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -40,17 +40,3 @@ $(document).on('ready turbolinks:load', function() {
 	  event.stopPropagation();
 	});
 });
-
-$(function() {
-	$('.input_icon.input_show_pwd').on('click', function(e) {
-		e.preventDefault();
-		e.stopPropagation();
-
-		var item = $('#' + $(this).data('id'));
-		if ( item.attr('type') == 'password' ) {
-			$('#' + $(this).data('id')).attr('type', 'input');
-		} else {
-			$('#' + $(this).data('id')).attr('type', 'password');
-		}
-	})
-})
